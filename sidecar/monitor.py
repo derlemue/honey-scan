f58c100303449abbbbd07dc5d40e2ceb0dbd738b
m3l1nda <ctf@lemue.org>
2026-01-08 01:47:35 +0100

    fix: revert timezone to +1h

--- sidecar/monitor.py
+++ sidecar/monitor.py
@@ -255,8 +255,8 @@ def update_threat_feed():
                              
                              # Force conversion to datetime if possible, or just add if it supports it
                              if final_dt:
-                                 # Add 3 hours (Heuristic adjustment based on observation)
-                                 new_time = final_dt + timedelta(hours=3)
+                                 # Add 1 hour (User Request: +1 is sufficient)
+                                 new_time = final_dt + timedelta(hours=1)
                                  time_display = str(new_time)
                              else:
                                  time_display = str(info_time)
